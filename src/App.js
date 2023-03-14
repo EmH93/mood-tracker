f3d1262db93a0ad47f75ce9e9e6c7ae043ec6de9
sonia-deni <sonia_deni@hotmail.com>
2023-03-14 21:28:03 +0000

    created navbar and router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Navbar from './components/NavBar/NavBar';
+import Home from './pages/Home/Home';
+import MoodDiary from './pages/MoodDiary/MoodDiary';
+import Resources from './pages/Resources/Resources';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <Router>
+        <Navbar>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/MoodDiary" element={<MoodDiary/>} />
+            <Route path="/Resources" element={<Resources/>} />
+          </Routes>
+        </Navbar>
+    </Router>
+    
   );
 }
 
---
+++ src/pages/MoodDiary/MoodDiary.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MoodDiary (){
+    return (
+        <div>
+            Mood Diary Page
+        </div>
+    )
+}
+
+export default MoodDiary;
