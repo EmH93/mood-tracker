a6348daeca87acf0e4b3e8cb1ff604155451c188
Your Name <you@example.com>
2023-03-15 19:32:47 +0000

    form handled

--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -1,36 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Form.css";
 
 function Form() {
+  const [selectedDate, setSelectedDate] = useState("");
+  const [selectedMood, setSelectedMood] = useState("");
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    localStorage.setItem(selectedDate, selectedMood);
+    setSelectedDate("");
+    setSelectedMood("");
+  };
+
   return (
     <div>
-      <form className="form" style={{ maxWidth: "400px" }}>
+      <form className="form" style={{ maxWidth: "400px" }} onSubmit={handleFormSubmit}>
         <div className="form-group">
           <label htmlFor="dateInput">Select a date:</label>
           <input
-            // value={formData.firstName}
             name="date"
-            // onChange={handleInputChange}
             type="date"
             className="form-control"
             id="dateInput"
             placeholder="YYYY-MM-DD"
             required
+            value={selectedDate}
+            onChange={(event) => setSelectedDate(event.target.value)}
           />
         </div>
 
         <div className="form-group">
           <label htmlFor="selectInput">Select mood:</label>
-          <select className="form-control" id="selectInput" required>
+          <select
+              className={`form-control ${
+                selectedMood === "option1"
+                  ? "selectedOption1"
+                  : selectedMood === "option2"
+                  ? "selectedOption2"
+                  :selectedMood === "option3"
+                  ? "selectedOption3": ""
+              }`}
+            id="selectInput"
+            required
+            value={selectedMood}
+            onChange={(event) => setSelectedMood(event.target.value)}
+          >
             <option value="">-- Select an option --</option>
-            <option value="option1">Option 1</option>
-            <option value="option2">Option 2</option>
-            <option value="option3">Option 3</option>
+            <option value="Stressed">Stressed</option>
+            <option value="Sad">Sad</option>
+            <option value="Chilled">Chilled</option>
           </select>
         </div>
 
-        {/* <button onClick={handleFormSubmit} className="btn btn-primary">
+        <button type="submit" className="btn btn-primary">
           Submit
-        </button> */}
+        </button>
       </form>
     </div>
   );
