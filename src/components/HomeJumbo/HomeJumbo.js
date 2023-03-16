b962f775c9d6dbb52cb46ca261c4937ab391bcef
Emma Hayward <emmahayward93@gmail.com>
2023-03-16 20:29:10 +0000

    Adding text to resource page, also changing id's on css so they target specific components

--- src/components/HomeJumbo/HomeJumbo.js
+++ src/components/HomeJumbo/HomeJumbo.js
@@ -9,10 +9,10 @@ function Jumbotron() {
     <div className="container-fluid" id="jumboWrapper">
         <motion.div initial={{ opacity: 0, scale: 0.8 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }}>
         <div className="jumbotron">
-        <h1 className="display-4">Track your mood</h1>
-        <p className="lead">Reflect, recognise and repair</p>
+        <h1 className="display-4" id='homeTitle'>Track your mood</h1>
+        <p className="lead" id='subHeadingHome'>Reflect, recognise and repair</p>
         <Link to="/MoodDiary">
-        <button className="btn btn-dark btn-lg">Get started</button>
+        <button className="btn btn-dark btn-lg" id='getStartedBtn'>Get started</button>
         </Link>
       </div> 
       </motion.div>
---
+++ src/components/ResourceText/ResourceText.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './ResourceText.css';
+
+function ResourceText(){
+
+    return(
+        <div>
+            <h1 id='resourceTitle'>So, why is tracking your mood so important?</h1>
+            <p id='resourceText'>Tracking your mood can help you to recognise patterns and changes in your moods, allowing you to better understand how you work and what you need to do to take better care of yourself.  For example, are you experiencing an extended period of stress? If so, it might be time to find some more support, or are you noticing a string of happy days? Perfect! Reflect on them, what is it that’s putting you in such a good state of mind?
+
+                Check out the resources below for more information on mood tracking and further resources for support.
+            </p>
+        </div>
+    )
+}
+
+export default ResourceText;
