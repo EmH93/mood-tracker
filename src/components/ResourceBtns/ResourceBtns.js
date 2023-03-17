ccf0718cb86b2ee8433152f8eb7d623dedf97fbf
Emma Hayward <emmahayward93@gmail.com>
2023-03-17 20:05:16 +0000

    Amended css for quote api

--- src/components/ResourceBtns/ResourceBtns.js
+++ src/components/ResourceBtns/ResourceBtns.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { motion } from 'framer-motion';
 import { Button, Space } from 'antd';
 import './ResourceBtns.css';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -7,13 +6,13 @@ import 'bootstrap/dist/css/bootstrap.css';
 function ResourceBtns() {
 
     return(
-        <motion.div>
+        <div>
             <Space wrap>
                 <Button className="buttonLink" href="https://blog.calm.com/blog/5-benefits-of-tracking-your-mood">Benefits of mood tracking</Button>
                 <Button className="buttonLink" href="https://www.nhs.uk/nhs-services/mental-health-services/">NHS mental health services</Button>
                 <Button className="buttonLink" href="https://www.headspace.com/science">Headspace mental health</Button>
             </Space>
-        </motion.div>
+        </div>
     )
 }
 
