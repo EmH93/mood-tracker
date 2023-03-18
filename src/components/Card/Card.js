6860faeb662e63e8031b841c25130df18547ac6c
Your Name <you@example.com>
2023-03-18 15:47:51 +0000

    Images added to cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,7 +2,10 @@ import React from "react";
 import { Avatar, Card, Col } from "antd";
 import "./Card.css";
 import stressed from './Moods/stressed.png';
-import happy from './Moods/happpy.png';
+import happy from './Moods/happy.png';
+import sad from './Moods/sad.png';
+import chilled from './Moods/chilled.png';
+import overjoyed from './Moods/overjoyed.png';
 const { Meta } = Card;
 
 function MoodCard(props) {
@@ -30,8 +33,14 @@ function MoodCard(props) {
           avatar={<Avatar className="avatar" src={`${
             props.title === "Stressed"
               ? stressed
+              : props.title === "Sad"
+              ? sad
               : props.title === "Happy"
               ? happy
+              : props.title === "Chilled"
+              ? chilled
+              : props.title === "Overjoyed"
+              ? overjoyed
               : ""
           }`}/>}
           title={props.title}
