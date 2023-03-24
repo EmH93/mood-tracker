9affdbb1bbef529c818fe08a98b6f09387dc9f78
sonia-deni <sonia_deni@hotmail.com>
2023-03-24 15:50:39 +0000

    added brackets so that the future time bug was fixed

--- src/pages/MoodDiary/MoodDiary.js
+++ src/pages/MoodDiary/MoodDiary.js
@@ -45,7 +45,7 @@ function MoodDiary() {
         const inputMinute = parseInt(selectedTime.substring(3,5));
 
 
-    if (((moment(inputDate).isBefore(currentDate)) || (moment(inputDate).isSame(currentDate))) && ((inputHour <= currentTimeHours) && (inputMinute <= currentTimeMinutes))){
+    if (((moment(inputDate).isBefore(currentDate)) || ((moment(inputDate).isSame(currentDate))) && ((inputHour <= currentTimeHours) && (inputMinute <= currentTimeMinutes)))){
         
           let moodObj = JSON.parse(localStorage.getItem("moodDiary") || "{}");
           moodObj[dateTime] = selectedMood;
